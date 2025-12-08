095c16d105ed9126fbb8c6c93e5904f6284fec68
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2025-12-08 13:37:32 +0600

    Subtraction of ASCII Digits

---
+++ Arithmetic-Operations/sub.asm
@@ -0,0 +1,31 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+A DB 35H
+B DB 31H
+
+.CODE  
+
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV AL, A
+    SUB AL, 30H
+
+    MOV BL, B
+    SUB BL, 30H
+
+    SUB AL, BL
+
+    ADD AL, 30H
+
+    MOV DL, AL
+    MOV AH, 2
+    INT 21H
+
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
