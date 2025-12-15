38aa3ad093ba46d87eb5dc8a191632a4c40fc50d
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2025-12-15 09:16:30 +0600

    Print a Character 10 Times Using LOOP

---
+++ Loops-Iteration/loop.asm
@@ -0,0 +1,24 @@
+.MODEL SMALL
+.STACK 100H  
+
+.DATA
+A DB "P$"
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV CX, 10
+
+PRINT:
+    LEA DX, A
+    MOV AH, 9
+    INT 21H
+    LOOP PRINT
+
+    MOV AH, 4CH
+    INT 21H
+    
+MAIN ENDP
+END MAIN
